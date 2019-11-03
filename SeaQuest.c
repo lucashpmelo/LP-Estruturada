db48a25e83e3cdd25126d80f868dfb2a3a37b11d
root <root@Lucas-PC.localdomain>
2019-11-03 14:38:45 -0200

    v1.1

--- SeaQuest.c
+++ SeaQuest.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <conio.h>
+#include <time.h>
+
+struct coordenada {
+    int x, y, deslocamento;
+};
 
 void gotoxy (int x, int y){
 COORD c;
@@ -11,13 +16,44 @@ SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),c);
 }
 
 char mapa[7][14];
+struct coordenada monstros[3];
 int px, py;
+
+
+void randomizaMonstro(){
+    int i;
+    srand (time(NULL));
+    for(i=0;i<=2;i++){
+        monstros[i].x = (rand()%5) + 1;
+        if((i%2) == 0){
+            monstros[i].y = 13;
+            monstros[i].deslocamento = -1;
+        }
+        else{
+            monstros[i].y = 0;
+            monstros[i].deslocamento = 1;
+        }
+    }
+}
+
+void movimentaMonstro(){
+    int i,x,y,des;
+    for(i=0;i<=2;i++){
+        x = monstros[i].x;
+        y = monstros[i].y;
+        des = monstros[i].deslocamento;
+        y = y + (des);
+        monstros[i].y = y;
+        mapa[x][y] = 'T';
+        mapa[x][y-(des)] = '°';
+    }
+}
 
 void inicia_Mapa(){
     int l, c;
     for(l=0;l<7;l++){
         for(c=0;c<14;c++){
-            mapa[l][c] = 'A';
+            mapa[l][c] = '°';
         }
     }
 }
@@ -62,16 +98,27 @@ main(){
     inicia_Mapa();
     mapa[px][py]='\0';
     Moldura();
+    randomizaMonstro();
     do{
         mostra_Mapa();
         tecla = getch();
-        mapa[px][py] = 'A';
+        mapa[px][py] = '°';
         switch(tecla){
-            case 'w': px--; break;
-            case 's': px++; break;
-            case 'd': py++; break;
-            case 'a': py--; break;
+            case 'w': if((px>0)&&(py>=0)){
+                px--;
+            } break;
+            case 's': if((px<6)&&(py<=13)){
+                px++;
+            } break;
+            case 'd': if((px>=0)&&(py<13)){
+                py++;
+            } break;
+            case 'a': if((px>=0)&&(py>0)){
+                py--;
+            } break;
         }
         mapa[px][py] = 'P';
+        movimentaMonstro();
+        //verificaColisoes();
     }while(tecla != 27);
 }
